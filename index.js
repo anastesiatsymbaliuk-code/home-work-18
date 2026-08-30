eca739b866bec49f94e977ac75f329a463be9a1f
Anastasiia Tsymbaliuk <anastesia.tsymbaliuk@gmail.com>
2026-08-30 13:50:04 +0200

    .

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /*   #1   */
-var myNum = 10;
+let myNum = 10;
 
 let myStr = 'some string';
 
 let myBool = true;
 
-var numbers = [1, 2, 3, 4, 5];
+let numbers = [1, 2, 3, 4, 5];
 
 const person = {
     first: 'First Name', 
     last: 'Last Name'
 };
 /*   #2   */
-var myNum = 10;
+let myNum = 10;
 myNum.name = 'decimal2';
 console.log(myNum.toFixed(2));
 /*   #3   */
